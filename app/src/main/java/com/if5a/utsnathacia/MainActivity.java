36c115b37ab4f43ee07504f0c95a64fc5f6fbe76
NathaciaLais <nathacialais@gmail.com>
2022-10-31 17:15:05 +0700

    Update 4

--- app/src/main/java/com/if5a/utsnathacia/MainActivity.java
+++ app/src/main/java/com/if5a/utsnathacia/MainActivity.java
@@ -6,12 +6,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.Spinner;
+
 
 
 
 public class MainActivity extends AppCompatActivity {
     private Button btndaftar;
+    private EditText etnama,etnomor;
+    private Spinner spjalur;
 
 
 
@@ -21,10 +24,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etnama = findViewById(R.id.et_nama);
+        etnomor = findViewById(R.id.et_nomor);
+        spjalur = findViewById(R.id.sp_jalur);
         btndaftar = findViewById(R.id.btn_daftar);
         btndaftar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String nama,nomor;
+
+                nama = etnama.getText().toString();
+                nomor = etnomor.getText().toString();
+                if (nama.trim().equals("")){
+                    etnama.setError("Nama tidak boleh kosong");
+                }
+                else if (nomor.trim().equals("")){
+                    etnomor.setError("Nomor tidak boleh kosong");
+                }
 
 
             }
