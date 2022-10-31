18ec1e7877dc36eefb8948e0e2f11ddcc1e4617b
NathaciaLais <nathacialais@gmail.com>
2022-10-31 15:12:10 +0700

    update 2

--- app/src/main/java/com/if5a/utsnathacia/MainActivity.java
+++ app/src/main/java/com/if5a/utsnathacia/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         btndaftar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this,"Konfirmasi Pendaftaran"), Toast.LENGTH_SHORT)
+                
 
             }
         });
