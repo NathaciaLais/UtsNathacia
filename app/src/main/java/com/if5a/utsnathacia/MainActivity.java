f2e9dfce1dc3b5dd61f27de41a10e32d49c17552
NathaciaLais <nathacialais@gmail.com>
2022-10-31 15:10:23 +0700

    Update 1

--- app/src/main/java/com/if5a/utsnathacia/MainActivity.java
+++ app/src/main/java/com/if5a/utsnathacia/MainActivity.java
@@ -3,18 +3,32 @@ package com.if5a.utsnathacia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 
 public class MainActivity extends AppCompatActivity {
-    
+    private Button btndaftar;
+
+
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btndaftar = findViewById(R.id.btn_daftar);
+        btndaftar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"Konfirmasi Pendaftaran"), Toast.LENGTH_SHORT)
+
+            }
+        });
 
 
     }
