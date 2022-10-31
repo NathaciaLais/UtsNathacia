b48fc4daf3c5f3a05f246c1db76d1622aaa9aeef
NathaciaLais <nathacialais@gmail.com>
2022-10-31 17:49:55 +0700

    Update 6

--- app/src/main/java/com/if5a/utsnathacia/MainActivity.java
+++ app/src/main/java/com/if5a/utsnathacia/MainActivity.java
@@ -24,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setTitle("Uts Nathacia Lais");
+
         etnama = findViewById(R.id.et_nama);
         etnomor = findViewById(R.id.et_nomor);
         spjalur = findViewById(R.id.sp_jalur);
